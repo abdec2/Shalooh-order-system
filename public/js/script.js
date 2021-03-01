59b21f4b62760bc66298a2e16cb70f8c220cbd59
abdec2 <abdec2@hotmail.com>
2021-03-01 17:15:44 +0300

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -23,7 +23,7 @@ if(document.querySelector('#btnCreateLbl') !== null)
                 const a = document.createElement('a');
                 a.style.display = 'none';
                 a.href = url;
-                a.download = document.querySelector('#orderID').value+''+getCurrentDate()+'.pdf';
+                a.download = document.querySelector('#orderID').value+'_'+getCurrentDate()+'.pdf';
                 document.body.appendChild(a);
                 a.click();
                 window.URL.revokeObjectURL(url);
