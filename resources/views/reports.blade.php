3aa7a9c066ce304023e1bc0ebb7607c241fd9a26
abdec2 <abdec2@hotmail.com>
2021-03-25 19:49:54 +0300

    Update reports.blade.php

--- resources/views/reports.blade.php
+++ resources/views/reports.blade.php
@@ -18,22 +18,20 @@
                                     <div class="flex flex-wrap">
                                         <div class="p-2 w-full sm:w-1/3">
                                             <div class="relative">
-                                                <input type="date" id="fromDate" name="fromDate"
-                                                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-yellow-500 focus:bg-white focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                                                    placeholder="Enter order number" required>
+                                                <label for="fromDate" class="block text-sm font-medium text-gray-700">Date From:</label>
+                                                <input type="date" id="fromDate" name="fromDate" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-yellow-500 focus:bg-white focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" placeholder="Enter order number" required>
                                             </div>
                                         </div>
                                         <div class="p-2 w-full sm:w-1/3">
                                             <div class="relative">
-                                                <input type="date" id="toDate" name="toDate"
-                                                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-yellow-500 focus:bg-white focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                                                    placeholder="Enter order number" required>
+                                                <label for="toDate" class="block text-sm font-medium text-gray-700">Date to:</label>
+                                                <input type="date" id="toDate" name="toDate" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-yellow-500 focus:bg-white focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" placeholder="Enter order number" required>
                                             </div>
                                         </div>
                                         <div class="p-2 w-full sm:w-1/3">
                                             <div class="relative">
-                                                <button
-                                                    class="text-white bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-md w-full">Submit</button>
+                                                <label class="block text-sm font-medium text-gray-700"> </label>
+                                                <button class="text-white bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-md w-full">Submit</button>
                                             </div>
                                         </div>
                                         @isset($type)
