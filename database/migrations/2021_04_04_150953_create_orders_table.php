8d6740305dd0bca41f60f7cc470304d9418e374c
abdec2 <abdec2@hotmail.com>
2021-05-25 14:51:59 +0300

    Update 2021_04_04_150953_create_orders_table.php

--- database/migrations/2021_04_04_150953_create_orders_table.php
+++ database/migrations/2021_04_04_150953_create_orders_table.php
@@ -19,11 +19,11 @@ class CreateOrdersTable extends Migration
             $table->string('customer_name', 255);
             $table->string('customer_contact', 50);
             $table->text('order_data')->nullable();
-            $table->integer('shipping_carrier_id')->nullable();
+            $table->bigInteger('shipping_carrier_id')->nullable();
             $table->foreign('shipping_carrier_id')->references('id')->on('shipping_carrier');
             $table->dateTime('order_date', $precision = 0);
             $table->string('payment_method', 100);
-            $table->integer('order_status_id');
+            $table->bigInteger('order_status_id');
             $table->foreign('order_status_id')->references('id')->on('order_status');
             $table->string('shipping_address1', 1000);
             $table->string('shipping_address2', 1000)->nullable();
