ec21ab7de9defdbd34296e455b5d78c668666f3b
abdec2 <abdec2@hotmail.com>
2021-07-23 20:39:33 +0300

    Update onreadyScripts.js

--- public/js/onreadyScripts.js
+++ public/js/onreadyScripts.js
@@ -774,31 +774,31 @@ const create_label = (orderID, order_number) => {
     .then(res=>{
         const contentType = res.headers.get("content-type");
         console.log(res);
-        if (contentType && contentType.indexOf("application/json") !== -1) {
-            return res.json().then(data => {
-              // process your JSON data further
-              alertify.alert(data.msg);
+        // if (contentType && contentType.indexOf("application/json") !== -1) {
+        //     return res.json().then(data => {
+        //       // process your JSON data further
+        //       alertify.alert(data.msg);
 
-            });
-          } else {
-            return res.blob().then(blob => {
+        //     });
+        //   } else {
+        //     return res.blob().then(blob => {
               
-                const url = window.URL.createObjectURL(blob);
-                const a = document.createElement('a');
-                a.style.display = 'none';
-                a.href = url;
-                if(blob.type == "application/zip"){
-                    a.download = order_number+'_'+getCurrentDate()+'.zip';
-                } else {
-                    a.download = order_number+'_'+getCurrentDate()+'.pdf';
-                }
-                document.body.appendChild(a);
-                a.click();
-                window.URL.revokeObjectURL(url);
-                window.location.reload();
+        //         const url = window.URL.createObjectURL(blob);
+        //         const a = document.createElement('a');
+        //         a.style.display = 'none';
+        //         a.href = url;
+        //         if(blob.type == "application/zip"){
+        //             a.download = order_number+'_'+getCurrentDate()+'.zip';
+        //         } else {
+        //             a.download = order_number+'_'+getCurrentDate()+'.pdf';
+        //         }
+        //         document.body.appendChild(a);
+        //         a.click();
+        //         window.URL.revokeObjectURL(url);
+        //         window.location.reload();
                 
-            });
-          }
+        //     });
+        //   }
     });
         
     // .then(result=>console.log(result));
